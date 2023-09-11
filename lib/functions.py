f9c7386879d0f7969e6356961276cd884a44bd0c
Tano Paul <tanopaul38@gmail.com>
2023-09-11 13:46:48 -0400

    completed functions lab python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+greet_programmer()
+greet('Tano')
+greet_with_default()
+print(add(1,2))
+print(halve(8))
